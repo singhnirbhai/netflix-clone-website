7ad146393825ea7f90a569510b32fe1d4e29c1d5
Nirbhai singh <120583283+singhnirbhai@users.noreply.github.com>
2026-06-05 00:35:23 +0530

    Update config.js

--- netflix-clone/src/config.js
+++ netflix-clone/src/config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 // craete a/c on themoviedb.org and replace access token below
 
 
-const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjUzYmIzMDY2ZWE0ZGYyMDE2YjJhYjczMmEwMGZiNiIsIm5iZiI6MTc1NTQyNjcxMi41NzcsInN1YiI6IjY4YTFhZjk4NWU4OTVlMjFhNjAyY2ZiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wfPY-8Y73_d9NVdIFss71eMu2JwWoSpnMG9Vv5_UKg0";
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDFjOTIzNmUwMzNmOTdiZDM4ZDZmYmIyMjE5NzE0NyIsIm5iZiI6MTc1NTQyNjcxMi41NzcsInN1YiI6IjY4YTFhZjk4NWU4OTVlMjFhNjAyY2ZiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9dpAe6hHbdQHhnYR-yMmRxEAkBMbYyexVSB1FG6fkDM";
 
 
   export {firebaseConfig, TMDB_Access_Key};
