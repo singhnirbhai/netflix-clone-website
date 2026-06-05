65b64b63bd6edb348792be7df3199de5f9336b51
Nirbhai singh <120583283+singhnirbhai@users.noreply.github.com>
2026-06-05 16:03:28 +0530

    Update Home.jsx

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <img src={hero_banner} alt="" className='banner-img'/>
         <div className="hero-caption">
           <img src={hero_title} alt="" className='caption-img'/>
-          <p>DevOps Is Best.</p>
+          <p>DevOps Is Best Linux,cloud,jenkins,openshift.</p>
           <div className="hero-btns">
             <button className='btn'><img src={play_icon} alt="" />Play</button>
             <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
