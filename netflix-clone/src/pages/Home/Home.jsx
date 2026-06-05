b6919d45e6406acfc2ad0f06230fcfa1db994793
Nirbhai singh <120583283+singhnirbhai@users.noreply.github.com>
2026-06-05 15:42:53 +0530

    Update Home.jsx

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
         <img src={hero_banner} alt="" className='banner-img'/>
         <div className="hero-caption">
           <img src={hero_title} alt="" className='caption-img'/>
-          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <p>DevOps Is Best.</p>
           <div className="hero-btns">
             <button className='btn'><img src={play_icon} alt="" />Play</button>
             <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
